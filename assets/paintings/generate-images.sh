88cfd5bc96462c5f0813665b89c40a93c6bef29e
Hugo Le Moine <hugo.le-moine@outlook.fr>
2020-10-14 18:43:50 +0200

    Added new paintings

--- assets/paintings/generate-images.sh
+++ assets/paintings/generate-images.sh
@@ -6,9 +6,13 @@ do
 	echo "${folder}"
 
 	for f in *.{png,PNG,jpg,JPG}
-		do magick convert $f -set filename:name %t -resize ${folder} -quality 80 ${folder}/%[filename:name].png
+		do magick convert $f -set filename:name %t -resize ${folder} -quality 80 ${folder}/%[filename:name].jpg
 	done
 done
 
 mkdir -p "fullsize"
-mv *.{png,PNG,jpg,JPG} fullsize/
+
+for f in *.{png,PNG,jpg,JPG}
+do magick convert $f -set filename:name %t fullsize/%[filename:name].jpg
+done
+rm *.{png,PNG,jpg,JPG}
